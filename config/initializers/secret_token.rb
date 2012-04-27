d96901acb956a5f769dee2f9ddcfdb24a49ecd47
Craig Wermert <craig@wermert>
2012-04-27 17:04:36 +0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babybrag::Application.config.secret_token = '4c15130f91759118c7d187ccb321b58384db5fa73ca8cb5a3c240aac26fc448e34f2ba764e2131a9e78957f34c494b194b760ead4774e81cb1a97e1f246f1703'
